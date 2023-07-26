63102f0c7128ca80e86ed114054d4bfc517b6ca4
Muhammed <muhammeddastan6926@icloud.com>
2023-07-26 12:43:47 +0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Muhammed Daştan on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
